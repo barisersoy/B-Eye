78d9f4b3a9ba5373db39224fc6d97887e4fca4fd
Baris Ersoy <baris.ersoy@olcsancad.com>
2022-06-08 12:26:01 +0300

    first commit

---
+++ mylib/config.py
@@ -0,0 +1,17 @@
+#===============================================================================
+""" Optional features config. """
+#===============================================================================
+# Enter mail below to receive real-time email alerts
+# e.g., 'email@gmail.com'
+MAIL = 'barisersoy@hotmail.com.tr'
+# Enter the ip camera url (e.g., url = 'http://191.138.0.100:8040/video')
+url = ''
+
+# ON/OFF for mail feature. Enter True to turn on the email alert feature.
+ALERT = True
+# Set max. people inside limit. Optimise number below: 10, 50, 100, etc.
+Threshold = 2
+# Auto run/Schedule the software to run at your desired time
+Scheduler = False
+#===============================================================================
+#===============================================================================
